8b7384d8082491383f163fa7b2a6c8d51f6be91a
Remi Huguet <hug211mire@gmail.com>
2020-04-06 21:19:11 +0200

    introduce cli logic with docopt

--- scabi/__main__.py
+++ scabi/__main__.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 # __main__.py
 
+"""cli
 
+usage:
+  cli.py sudo <pms> install <package> [--verbose]
+  cli.py <pms> install <package>
+  cli.py <pms> install <package> -s <filename> 
+  cli.py --version
 
+options:
+  -v --verbose      Show full output.
+  -a --all          Search vulnerabilities through all source (MITRE, OSS for now)
+  -o --oss          Search vulnerabilities through OSS
+  -m --mitre        Search vulnerabilities through MITRE
+  -s --save         Save output to file
+  -h --help         Show this screen.
+
+"""
+
+from docopt import docopt
 from scabi import utility_pms
 from scabi import utility_cli
 
@@ -19,35 +36,39 @@ def main():
     Implement CLI logic 
     """
 
+    arguments = docopt(__doc__, version='scabi 1.0.1')
+    print(arguments)
+
     pms = utility_pms.get_pms_name()
-    package = utility_pms.get_package_name()
+    # package = utility_pms.get_package_name()
     
+    __verbose = arguments["--verbose"]
 
     if pms == 'apt' or pms == 'apt-get' : 
         deps = apt_scan.get_apt_dependencies()
         utility_cli.print_dependencies(deps)
-        utility_cli.OSS_print_vulnerabiliies(deps)
+        utility_cli.OSS_print_vulnerabiliies(deps,__verbose)
 
     elif pms == 'composer'  : 
         deps = composer_scan.get_composer_dependencies()
         utility_cli.print_dependencies(deps)
-        utility_cli.OSS_print_vulnerabiliies(deps)
+        utility_cli.OSS_print_vulnerabiliies(deps,__verbose)
 
     elif pms == 'gem'       : 
         deps = gem_scan.get_gem_dependencies()
         utility_cli.print_dependencies(deps)
-        utility_cli.OSS_print_vulnerabiliies(deps)
+        utility_cli.OSS_print_vulnerabiliies(deps,__verbose)
 
     elif pms == 'npm'       : 
         deps = npm_scan.get_npm_dependencies()
         utility_cli.print_dependencies(deps)
-        utility_cli.OSS_print_vulnerabiliies(deps)
+        utility_cli.OSS_print_vulnerabiliies(deps,__verbose)
 
     elif pms == 'pip'       : 
         deps = pip_scan.get_pip_dependencies()
         utility_cli.print_dependencies(deps)
-        utility_cli.OSS_print_vulnerabiliies(deps)
-        utility_cli.MITRE_print_vulnerabilites(deps)
+        utility_cli.OSS_print_vulnerabiliies(deps,__verbose)
+        utility_cli.MITRE_print_vulnerabilites(deps,__verbose)
 
     else : 
         print("Your PMS is not supported for the moment")
